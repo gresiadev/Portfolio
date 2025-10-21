a9cf5ec62289002406d9a488805c0398b02c006d
Lucas <lucasgresia@hotmail.com>
2025-10-21 17:15:39 -0300

    Actualización de información de componentes, arreglos de tamaños de caja en responsive

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,10 +14,16 @@ function Projects() {
                <article className="portfolio__project-card">
                   <h3 className="portfolio__project-title">User Manager</h3>
                   <img src="public/assets/images/proyects/user2udt.png"
-                     alt="Project userManager img"
+                     alt="Project userManager img*"
                      className="portfolio__project-img"
                   />
+
                   <div className="portfolio__project-description-container">
+                     <div className="portfolio__project-description">
+                        <p className="portfolio__project-description-text">
+                           Aplicación para gestionar usuarios, con autenticación, rutas protegidas y formularios. Simula la creación, edición y eliminación de usuarios, obteniendo los datos desde la API de JSONPlaceholder. Más info en la App.
+                        </p>
+                     </div>
                      <ul className="portfolio__project-techs-list">
                         <li className="portfolio__project-techs-item">React js</li>
                         <li className="portfolio__project-techs-item">React Router Dom</li>
@@ -25,11 +31,7 @@ function Projects() {
                         <li className="portfolio__project-techs-item">Fetch API</li>
                         <li className="portfolio__project-techs-item">Form validation</li>
                      </ul>
-                     <div className="portfolio__project-description">
-                        <p className="portfolio__project-description-text">
-                           Aplicación para gestionar usuarios, con autenticación, rutas protegidas y formularios. Simula la creación, edición y eliminación de usuarios, obteniendo los datos desde la API de JSONPlaceholder. Más info en la App.
-                        </p>
-                     </div>
+
                   </div>
                   <div className="portfolio__project-btns">
                      <NavLink
@@ -38,10 +40,6 @@ function Projects() {
                      >
                         Ver proyecto
                      </NavLink>
-                     <a
-                        href="https://github.com/gresiadev/userManager"
-                        className="portfolio__project-btn"
-                     >Ver código</a>
                   </div>
                </article>
             </li>
@@ -50,22 +48,25 @@ function Projects() {
             <li className="portfolio__projects-item">
                <article className="portfolio__project-card">
                   <h3 className="portfolio__project-title">League Team Builder</h3>
-                  <img src="public/assets/images/proyects/lol2udt.png" alt=""
+                  <img src="public/assets/images/proyects/lol2udt.png" alt="Team league buider img*"
                      className="portfolio__project-img"
                   />
+
                   <div className="portfolio__project-description-container">
-                     <ul className="portfolio__project-techs-list">
-                        <li className="portfolio__project-techs-item">React js</li>
-                        <li className="portfolio__project-techs-item">React Router Dom</li>
-                        <li className="portfolio__project-techs-item">Redux Toolkit</li>
-                        <li className="portfolio__project-techs-item">Persistencia</li>
-                     </ul>
+
                      <div className="portfolio__project-description">
                         <p className="portfolio__project-description-text">
                            Aplicación para armar equipo personalizado de League of Legends. Permite visualizar campeones, filtrarlos por tipo o poder, y agregarlos o quitarlos del equipo. Más info en la App.
                         </p>
                      </div>
+                     <ul className="portfolio__project-techs-list">
+                        <li className="portfolio__project-techs-item">React js</li>
+                        <li className="portfolio__project-techs-item">React Router Dom</li>
+                        <li className="portfolio__project-techs-item">Redux Toolkit</li>
+                        <li className="portfolio__project-techs-item">Persistencia</li>
+                        <li className="portfolio__project-techs-item">Filtros</li>
 
+                     </ul>
                   </div>
                   <div className="portfolio__project-btns">
                      <NavLink
@@ -74,9 +75,6 @@ function Projects() {
                      >
                         Ver proyecto
                      </NavLink>
-                     <a href="https://github.com/gresiadev/LeagueTeamBuilder"
-                        className="portfolio__project-btn"
-                     >Ver código</a>
                   </div>
                </article>
             </li>
